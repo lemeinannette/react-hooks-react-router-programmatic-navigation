f14468a8128224cc248761ac68b5e3702a51327f
Lemein <lemeinannette@gmail.com>
2023-11-04 09:58:02 +0300

    completed lab

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,119 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect, Switch, useHistory } from "react-router-dom";
 
-function Login({ setIsLoggedIn }) {
+// Mock user data and authentication status
+const fakeUserData = {
+  username: "user123",
+  password: "password123",
+};
+
+function App() {
+  const [user, setUser] = useState(null);
   const history = useHistory();
+
+  // Function to handle login
+  const handleLogin = () => {
+    setUser(fakeUserData);
+    history.push("/home");
+  };
+
+  // Function to handle logout
+  const handleLogout = () => {
+    setUser(null);
+    history.push("/login");
+  };
+
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/home">
+            {user ? (
+              <Home isSignedIn={true} />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </Route>
+          <Route path="/login">
+            {user ? (
+              <Redirect to="/home" />
+            ) : (
+              <Login onLogin={handleLogin} />
+            )}
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home({ isSignedIn }) {
+  if (!isSignedIn) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Your home page content here */}
+    </div>
+  );
+}
+
+function Login({ onLogin }) {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
 
-  function handleChange(e) {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
-  }
+    if (formData.username === fakeUserData.username && formData.password === fakeUserData.password) {
+      onLogin();
+    } else {
+      alert("Invalid username or password");
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <input
-        type="text"
-        name="username"
-        value={formData.username}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={formData.username}
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={formData.password}
+          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+        />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
   );
 }
 
-export default Login;
+export default App;
